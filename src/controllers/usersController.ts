ff29e74225e50eabc47bdfc77ae47f7b211a0b62
Marcello <marcelloaltermundi@gmail.com>
2023-03-17 16:57:58 -0300

    refactor: arquivo tokenGenerator para classe

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import tokenGenerator from '../utils/tokenGenerator';
+import TokenGenerator from '../utils/tokenGenerator';
 import { IToken } from '../utils/interfaces';
 import UsersService from '../services/usersService';
 
@@ -19,7 +19,8 @@ export default class {
     if (type === 'NOT_FOUND') return res.status(400).json({ message });
     if (type === 'INVALID') return res.status(422).json({ message });
 
-    const token = tokenGenerator(username);
+    const tokenGenerator = new TokenGenerator(username);
+    const token = tokenGenerator.generate();
   
     return res.status(201).json({ token });
   };
@@ -32,7 +33,8 @@ export default class {
     if (type === 'NOT_FOUND') return res.status(400).json({ message });
     if (type === 'INVALID') return res.status(401).json({ message });
 
-    const token = tokenGenerator(username);
+    const tokenGenerator = new TokenGenerator(username);
+    const token = tokenGenerator.generate();
 
     return res.status(200).json({ token });
   };
--- src/utils/tokenGenerator.ts
+++ src/utils/tokenGenerator.ts
@@ -7,6 +7,22 @@ const jwtConfig: SignOptions = {
   algorithm: 'HS256',
 };
 
-const tokenGenerator = (user: string): string => jwt.sign({ data: user }, secret, jwtConfig);
+// const tokenGenerator = (user: string): string => jwt.sign({ data: user }, secret, jwtConfig);
 
-export default tokenGenerator;
+// export default tokenGenerator;
+
+export default class TokenGenerator {
+  private secret: typeof secret;
+
+  private jwtConfig: typeof jwtConfig;
+
+  private user: string;
+
+  constructor(user: string) {
+    this.secret = secret;
+    this.jwtConfig = jwtConfig;
+    this.user = user;
+  }
+
+  public generate = (): string => jwt.sign({ data: this.user }, secret, jwtConfig);
+}
